dd9babb454378e78c3c9a393031a15c77015631a
Shashank Garg <9510835+shashankgarg1@users.noreply.github.com>
2018-12-08 17:47:36 -0500

    change loss to only l1 and decreased lr

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -92,7 +92,9 @@ class Pix2PixModel(BaseModel):
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
 
-        self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        # replaced the below line (remove g_gan loss as suggested by nikoss)
+        # self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        self.loss_G = self.loss_G_L1
 
         self.loss_G.backward()
 
