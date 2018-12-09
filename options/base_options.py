945a1850903a27a82b37db72cd6c42e6e7bb923d
Jagtar Singh <jagtarsingh@Jagtars-MacBook-Pro.local>
2018-12-08 22:31:49 -0500

    changed folder name

--- options/base_options.py
+++ options/base_options.py
@@ -24,7 +24,7 @@ class BaseOptions():
         parser.add_argument('--netG', type=str, default='resnet_3blocks', help='selects model to use for netG')
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        parser.add_argument('--name', type=str, default='CUHK_pix2pix', help='name of the experiment. It decides where to store samples and models')
+        parser.add_argument('--name', type=str, default='sydney_house_pix2pix', help='name of the experiment. It decides where to store samples and models')
         parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded. [unaligned | aligned | single]')
         parser.add_argument('--model', type=str, default='pix2pix', help='chooses which model to use. cycle_gan, pix2pix, test')
         parser.add_argument('--direction', type=str, default='BtoA', help='AtoB or BtoA')
