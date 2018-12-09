e14e9c6e7c45a2da70e5e1442eb43066c911709f
Jagtar Singh <jagtarsingh@vagelos-ve507-0509.apn.wlan.upenn.edu>
2018-12-09 15:37:01 -0500

    Changed model to unet_128, lr to 0.0002, epochs to 500

--- options/base_options.py
+++ options/base_options.py
@@ -11,7 +11,7 @@ class BaseOptions():
         self.initialized = False
 
     def initialize(self, parser):
-        parser.add_argument('--dataroot', type = str, default = '/content/gdrive/My Drive/sydney_house_data', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
+        parser.add_argument('--dataroot', type = str, default = './sydney_house_data', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
         parser.add_argument('--loadSize', type=int, default=286, help='scale images to this size')
         parser.add_argument('--fineSize', type=int, default=256, help='then crop to this size')
@@ -21,7 +21,7 @@ class BaseOptions():
         parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in first conv layer')
         parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in first conv layer')
         parser.add_argument('--netD', type=str, default='basic', help='selects model to use for netD')
-        parser.add_argument('--netG', type=str, default='resnet_3blocks', help='selects model to use for netG')
+        parser.add_argument('--netG', type=str, default='unet_128', help='selects model to use for netG')
         parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--name', type=str, default='sydney_house_pix2pix', help='name of the experiment. It decides where to store samples and models')
